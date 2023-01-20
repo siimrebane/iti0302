3ef9686dc9552547c7110fdc3b5c81b09baf358e
Siim Rebane <siim@rebane.org>
2023-01-20 11:15:39 +0200

    initial commit

---
+++ src/main/java/ee/taltech/iti0302/Iti0302Application.java
@@ -0,0 +1,13 @@
+package ee.taltech.iti0302;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iti0302Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Iti0302Application.class, args);
+    }
+
+}
